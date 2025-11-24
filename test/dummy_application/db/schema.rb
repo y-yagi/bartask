0cf1154b159b209b6817dda3009e953c4fc56544
Yuji Yaginuma <yuuji.yaginuma@gmail.com>
2025-11-24 11:49:07 +0900

    Bump dependencies

--- test/dummy_application/db/schema.rb
+++ test/dummy_application/db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_11_030140) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
+ActiveRecord::Schema[8.1].define(version: 2024_12_11_030140) do
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.string "email"
     t.datetime "updated_at", null: false
   end
 end
